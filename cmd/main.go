1f7331b4f26901b4c4d5a04b917f569a1c9a509b
sjuuun <osjuny11@gmail.com>
2021-08-04 00:48:46 +0900

    fix: using promptui to select config from multiple request configs

--- cmd/main.go
+++ cmd/main.go
@@ -3,8 +3,10 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/go-requ/requ/model"
 	"github.com/go-requ/requ/network"
 	"github.com/go-requ/requ/parser"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
@@ -15,7 +17,34 @@ func main(cmd *cobra.Command, args []string) {
 		fmt.Println(configList)
 	}
 
-	for _, config := range configList {
-		network.Request(config)
+	config, err := selectFromConfigList(configList)
+	network.Request(config)
+}
+
+func selectFromConfigList(configList []model.HttpRequestConfig) (model.HttpRequestConfig, error) {
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ .Method }}  {{ .Url }}",
+		Active:   "\U00002705 {{ .Method | cyan }} ({{ .Url | red }})",
+		Inactive: "  {{ .Method | cyan }} ({{ .Url | red }})",
+		Selected: "\U00002705 {{ .Method }} {{ .Url }}",
+		Details: `
+--------- Config ----------
+{{ "Method:" | faint }}	{{ .Method }}
+{{ "Url:" | faint }}	{{ .Url }}
+{{ "Headers:" | faint }} {{ .Headers }}
+{{ "Body:" | faint }} {{ .Body }}`,
 	}
+
+	prompt := promptui.Select{
+		Label:     "Select Request",
+		Items:     configList,
+		Templates: templates,
+		Size:      len(configList),
+	}
+
+	i, _, err := prompt.Run()
+	if err != nil {
+		return configList[0], err
+	}
+	return configList[i], nil
 }
