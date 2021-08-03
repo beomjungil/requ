c25d751fea5aab4b684e3e8d4d7ac0ba3e620334
sjuuun <osjuny11@gmail.com>
2021-08-03 23:40:04 +0900

    feat: combine file parser and HTTP request sender

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,19 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/go-requ/requ/network"
 	"github.com/go-requ/requ/parser"
 	"github.com/spf13/cobra"
 )
 
 func main(cmd *cobra.Command, args []string) {
 	var filePath string = args[0]
-	var config, err = parser.Parse(filePath)
+	var configList, err = parser.Parse(filePath)
+	if err != nil {
+		fmt.Println(configList)
+	}
 
-	if err == nil {
-		fmt.Println(config)
+	for _, config := range configList {
+		network.Request(config)
 	}
 }
