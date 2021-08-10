9eb9d77735a1a675944f71f35f5cea38dc78e36c
Seungjun Oh <47017729+sjuuun@users.noreply.github.com>
2021-08-10 19:15:44 +0900

    feat: combine parser and request sender (#8)

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,48 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/go-requ/requ/model"
+	"github.com/go-requ/requ/network"
 	"github.com/go-requ/requ/parser"
+	"github.com/manifoldco/promptui"
 	"github.com/spf13/cobra"
 )
 
 func main(cmd *cobra.Command, args []string) {
 	var filePath string = args[0]
-	var config, err = parser.Parse(filePath)
-
-	if err == nil {
-		fmt.Println(config)
+	var configList, err = parser.Parse(filePath)
+	if err != nil {
+		fmt.Println(configList)
 	}
+
+	config, err := selectFromConfigList(configList)
+	network.Request(config)
+}
+
+func selectFromConfigList(configList []model.HttpRequestConfig) (model.HttpRequestConfig, error) {
+	templates := &promptui.SelectTemplates{
+		Label:    "{{ .Method }}  {{ .Url }}",
+		Active:   "\U00002705 {{ .Method | cyan }} ({{ .Url | red }})",
+		Inactive: "  {{ .Method | cyan }} ({{ .Url | red }})",
+		Selected: "\U00002705 {{ .Method }} {{ .Url }}",
+		Details: `
+--------- Config ----------
+{{ "Method:" | faint }}	{{ .Method }}
+{{ "Url:" | faint }}	{{ .Url }}
+{{ "Headers:" | faint }} {{ .Headers }}
+{{ "Body:" | faint }} {{ .Body }}`,
+	}
+
+	prompt := promptui.Select{
+		Label:     "Select Request",
+		Items:     configList,
+		Templates: templates,
+		Size:      len(configList),
+	}
+
+	i, _, err := prompt.Run()
+	if err != nil {
+		return configList[0], err
+	}
+	return configList[i], nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/go-requ/requ
 go 1.16
 
 require (
+	github.com/manifoldco/promptui v0.8.0 // indirect
 	github.com/spf13/cobra v1.2.1
 	github.com/stretchr/testify v1.7.0 // indirect
 )
